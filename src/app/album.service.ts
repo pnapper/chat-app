4a706edc5a19ea74cb98272badc78b88dd62d53f
alvin niamatali and patrick napper <patrick@>
2017-12-12 11:23:05 -0800

    add delete album function.

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -28,4 +28,9 @@ export class AlbumService {
                                 artist: localUpdatedAlbum.artist,
                                 description: localUpdatedAlbum.description});
   }
+
+  deleteAlbum(localAlbumToDelete){
+    var albumEntryInFirebase = this.getAlbumById(localAlbumToDelete.$key);
+    albumEntryInFirebase.remove();
+  }
 }
